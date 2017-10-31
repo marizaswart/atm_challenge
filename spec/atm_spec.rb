1355ceecb27e563357a01d7d54166a53ac053d5a
marizaswart <mariza.swart@gmail.com>
2017-10-31 14:32:33 +0100

    Remove extra attribute for account before refacture the code

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -12,10 +12,7 @@ describe Atm do
   it 'has 1000$ on initialize' do
     expect(subject.funds).to eq 1000
   end
-  before do
-    allow(account).to receive(:balance).and_return(100)
-    allow(account).to receive(:balanse=)
-  end
+
   it 'funds are reduced at withdraw' do
     subject.withdraw(50, account)
     expect(subject.funds).to eq 950
