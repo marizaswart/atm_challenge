53db2e193221ab68c241650942e0ced4e4da6d31
marizaswart <mariza.swart@gmail.com>
2017-11-02 13:17:30 +0100

    Account status should check if the account is active (use a symbol) it should also be able to be deactivated

--- lib/account.rb
+++ lib/account.rb
@@ -2,14 +2,19 @@
 class Account
   STANDARD_VALIDITY_YRS = 5
 
-  attr_accessor :pin_code
+  attr_accessor :pin_code, :account_status
 
   def initialize
     @pin_code = generate_pin
+    @account_status = :active
   end
 
   def exp_date
     Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+  end
+
+  def deactivate
+    @account_status = :deactivated
   end
 
   private
